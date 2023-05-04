b361d880dfbe1151dd6c039a90ee246a8a4ddfe2
romashorodok <108732029+romashorodok@users.noreply.github.com>
2023-05-04 15:38:59 +0300

    feat: Added base crud models

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,2 +1,8 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('image') }}"><i class="nav-icon la la-question"></i> Images</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('bucket') }}"><i class="nav-icon la la-question"></i> Buckets</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('album') }}"><i class="nav-icon la la-question"></i> Albums</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('audio') }}"><i class="nav-icon la la-question"></i> Audio</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('genre') }}"><i class="nav-icon la la-question"></i> Genres</a></li>
