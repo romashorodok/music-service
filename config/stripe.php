6c954a3a27ba7eedaf06c59074a87f0df94937b2
romashorodok <108732029+romashorodok@users.noreply.github.com>
2023-05-25 13:08:22 +0300

    ref: remove

--- config/stripe.php
+++ config/stripe.php
@@ -7,11 +7,9 @@ return [
     'products' => [
         'extended'  => [
             'product' => env('STRIPE_EXTENDED_PRODUCT'),
-            'price'   => 'price_1N6WIzAgTou10Ln8kvK1s1R2',
         ],
         'unlimited' => [
             'product' => env('STRIPE_UNLIMITED_PRODUCT'),
-            'price'   => 'price_1N6WKWAgTou10Ln8ZWxPXZ2g',
         ],
     ]
 ];
