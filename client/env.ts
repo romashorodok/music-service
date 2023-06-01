f9401fd972cb651d634c7c10319028d9bad426a4
romashorodok <108732029+romashorodok@users.noreply.github.com>
2023-06-01 17:23:08 +0300

    feat: Run client on docker container

--- client/env.ts
+++ client/env.ts
@@ -1,4 +1,5 @@
 export const API_HOST = process.env.NEXT_PUBLIC_API;
+export const API_INTERNAL_HOST = process.env.NEXT_PUBLIC_INTERNAL_API;
 export const NEXT_HOST = "http://localhost:3000";
 export const STRIPE_PUBLISH_KEY = process.env.NEXT_PUBLIC_STRIPE_KEY;
 
